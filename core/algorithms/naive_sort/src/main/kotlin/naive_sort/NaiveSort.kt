5653d5784212e9a48ce373f53c7ec76996bf0c8e
yorche3 <hyaoki123@gmail.com>
2026-09-18 12:13:20 -0600

    feat(algorithms): add naive sort implementation

--- core/algorithms/naive_sort/src/main/kotlin/naive_sort/NaiveSort.kt
+++ core/algorithms/naive_sort/src/main/kotlin/naive_sort/NaiveSort.kt
@@ -12,4 +12,59 @@
 
 package naive_sort
 
-object NaiveSort
+object NaiveSort {
+    fun selectionSort(arr: IntArray?): IntArray? {
+        if (arr == null) return null
+        val n = arr.size
+        if (n <= 1) return arr
+        for (i in 0 until n - 1) {
+            var minIndex = i
+            for (j in i + 1 until n) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j
+                }
+            }
+            if (minIndex != i) {
+                val temp = arr[i]
+                arr[i] = arr[minIndex]
+                arr[minIndex] = temp
+            }
+        }
+        return arr
+    }
+
+    fun bubbleSort(arr: IntArray?): IntArray? {
+        if (arr == null) return null
+        val n = arr.size
+        if (n <= 1) return arr
+        for (i in 0 until n - 1) {
+            var swapped = false
+            for (j in 0 until n - i - 1) {
+                if (arr[j] > arr[j + 1]) {
+                    val temp = arr[j]
+                    arr[j] = arr[j + 1]
+                    arr[j + 1] = temp
+                    swapped = true
+                }
+            }
+            if (!swapped) break
+        }
+        return arr
+    }
+
+    fun insertionSort(arr: IntArray?): IntArray? {
+        if (arr == null) return null
+        val n = arr.size
+        if (n <= 1) return arr
+        for (i in 1 until n) {
+            val key = arr[i]
+            var j = i - 1
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j]
+                j--
+            }
+            arr[j + 1] = key
+        }
+        return arr
+    }
+}
